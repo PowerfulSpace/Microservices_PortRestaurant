e8a46005ee763bd2b96dc11014f5dec84dddd838
Alexander <sany199574@gmail.com>
2023-12-30 23:50:08 +0300

    Добавлена модель  - Product

---
+++ src/PortRestaurant/PS.PortRestaurant.Services.ProductAPI/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PS.PortRestaurant.Services.ProductAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; } = null!;
+        [Range(1, 1000)]
+        public decimal Price { get; set; }
+        public string Description { get; set; } = string.Empty;      
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
