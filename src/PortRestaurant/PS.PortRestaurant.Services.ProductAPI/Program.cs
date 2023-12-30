2441521ba5f292231b5454e5c04483f9a46c57e9
Alexander <sany199574@gmail.com>
2023-12-31 00:16:40 +0300

    Добавлена локализация в проект  - PS.PortRestaurant.Services.ProductAPI

--- src/PortRestaurant/PS.PortRestaurant.Services.ProductAPI/Program.cs
+++ src/PortRestaurant/PS.PortRestaurant.Services.ProductAPI/Program.cs
@@ -3,15 +3,34 @@
 
 using Microsoft.EntityFrameworkCore;
 using PS.PortRestaurant.Services.ProductAPI.DbContexts;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddDbContext<ApplicationDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+
+var localizationOptions = new RequestLocalizationOptions();
+
+var supportedCultures = new[]
+{
+    new CultureInfo("en-US"),
+    new CultureInfo("es-ES")
+};
+
+localizationOptions.SupportedCultures = supportedCultures;
+localizationOptions.SupportedUICultures = supportedCultures;
+localizationOptions.SetDefaultCulture("en-US");
+localizationOptions.ApplyCurrentCultureToResponseHeaders = true;
+
+
+
 
 var app = builder.Build();
 
@@ -21,6 +40,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+app.UseRequestLocalization(localizationOptions);
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
