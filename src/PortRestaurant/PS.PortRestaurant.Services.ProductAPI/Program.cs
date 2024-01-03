c2d31c1992c6892f2f165274a04de9897e8879db
Alexander <sany199574@gmail.com>
2024-01-04 01:44:45 +0300

    Настроил автомаппер в Program

--- src/PortRestaurant/PS.PortRestaurant.Services.ProductAPI/Program.cs
+++ src/PortRestaurant/PS.PortRestaurant.Services.ProductAPI/Program.cs
@@ -1,13 +1,20 @@
 
 
 
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using PS.PortRestaurant.Services.ProductAPI;
 using PS.PortRestaurant.Services.ProductAPI.DbContexts;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddDbContext<ApplicationDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
