06a6fcfc7ae0d95f8cbc921502bb5357b36f7c3f
Alexander <sany199574@gmail.com>
2024-01-04 02:40:19 +0300

    Добавлен контроллер  - ProductAPIController

---
+++ src/PortRestaurant/PS.PortRestaurant.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PS.PortRestaurant.Services.ProductAPI.Controllers
+{
+    public class ProductAPIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
