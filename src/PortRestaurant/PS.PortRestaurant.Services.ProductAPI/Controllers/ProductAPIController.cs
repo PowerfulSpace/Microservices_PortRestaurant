7ddc48c03ec32315b43485dd20c56ede3bae5eec
Alexander <sany199574@gmail.com>
2024-01-04 02:57:50 +0300

    Добавлен в контроллер метод  - Get

--- src/PortRestaurant/PS.PortRestaurant.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ src/PortRestaurant/PS.PortRestaurant.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PS.PortRestaurant.Services.ProductAPI.Models.Dto;
+using PS.PortRestaurant.Services.ProductAPI.Repository;
 
 namespace PS.PortRestaurant.Services.ProductAPI.Controllers
 {
-    public class ProductAPIController : Controller
+    [Route("api/products")]
+    public class ProductAPIController : ControllerBase
     {
-        public IActionResult Index()
+
+        protected ResponseDto _response;
+        private IProductRepository _productRepository;
+
+        public ProductAPIController(ProductRepository productRepository)
         {
-            return View();
+            _productRepository = productRepository;
+            this._response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _productRepository.GetProducts();
+                _response.Result = productDtos;
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { e.ToString() };
+            }
+
+            return _response;
         }
     }
 }
