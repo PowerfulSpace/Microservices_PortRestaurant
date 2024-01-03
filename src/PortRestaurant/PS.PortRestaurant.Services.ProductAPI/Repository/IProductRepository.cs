07dfb92b464d27da52f7818969eb97533032da55
Alexander <sany199574@gmail.com>
2024-01-04 01:16:15 +0300

    Реализован интерфейс  - IProductRepository

--- src/PortRestaurant/PS.PortRestaurant.Services.ProductAPI/Repository/IProductRepository.cs
+++ src/PortRestaurant/PS.PortRestaurant.Services.ProductAPI/Repository/IProductRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace PS.PortRestaurant.Services.ProductAPI.Repository
+﻿using PS.PortRestaurant.Services.ProductAPI.Models.Dto;
+
+namespace PS.PortRestaurant.Services.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(Guid id);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(Guid id);
     }
 }
