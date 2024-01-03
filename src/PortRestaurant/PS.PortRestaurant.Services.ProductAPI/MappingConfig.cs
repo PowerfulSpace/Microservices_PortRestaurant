47eb3c977fe6497c3c11b2e6b4f841467a8b2c05
Alexander <sany199574@gmail.com>
2024-01-04 01:40:34 +0300

    Добавлен  класс конфигурации для атомаппинга  - MappingConfig

---
+++ src/PortRestaurant/PS.PortRestaurant.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using PS.PortRestaurant.Services.ProductAPI.Models;
+using PS.PortRestaurant.Services.ProductAPI.Models.Dto;
+
+namespace PS.PortRestaurant.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>();
+                config.CreateMap<Product, ProductDto>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
