4f53a02892186f815229cfdbf7fca971b7ec8521
Alexander <sany199574@gmail.com>
2024-01-06 04:18:25 +0300

    Добавлен интерфейс - IProductService

---
+++ src/PortRestaurant/PS.PortRestaurant.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,11 @@
+﻿namespace PS.PortRestaurant.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        public Task<T> GetAllProductsAsync<T>();
+        public Task<T> GetProductAsync<T>(Guid id);
+        public Task<T> CreateProductAsync<T>();
+        public Task<T> UpdateProductAsync<T>();
+        public Task<T> DeleteProductAsync<T>(Guid id);
+    }
+}
