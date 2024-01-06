12fb5ab60d48ad46212bd1aebee90852cb57695c
Alexander <sany199574@gmail.com>
2024-01-06 04:33:31 +0300

    Изменён интерфейс сервиса  - IProductService

--- src/PortRestaurant/PS.PortRestaurant.Web/Services/IServices/IProductService.cs
+++ src/PortRestaurant/PS.PortRestaurant.Web/Services/IServices/IProductService.cs
@@ -1,11 +1,13 @@
-﻿namespace PS.PortRestaurant.Web.Services.IServices
+﻿using PS.PortRestaurant.Web.Models.Dto;
+
+namespace PS.PortRestaurant.Web.Services.IServices
 {
     public interface IProductService
     {
         public Task<T> GetAllProductsAsync<T>();
         public Task<T> GetProductAsync<T>(Guid id);
-        public Task<T> CreateProductAsync<T>();
-        public Task<T> UpdateProductAsync<T>();
+        public Task<T> CreateProductAsync<T>(ProductDto productDto);
+        public Task<T> UpdateProductAsync<T>(ProductDto productDto);
         public Task<T> DeleteProductAsync<T>(Guid id);
     }
 }
