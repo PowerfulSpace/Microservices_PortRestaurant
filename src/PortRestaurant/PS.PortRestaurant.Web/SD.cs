b9ecda7670491b82cc911e18b94aae0b26c98b25
Alexander <sany199574@gmail.com>
2024-01-06 04:02:26 +0300

    Добавлен статический класс для статических деталей проекта

---
+++ src/PortRestaurant/PS.PortRestaurant.Web/SD.cs
@@ -0,0 +1,17 @@
+﻿namespace PS.PortRestaurant.Web
+{
+    public static class SD
+    {
+
+        public static string ProductAPIBase { get; set; } = string.Empty;
+
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+
+    }
+}
