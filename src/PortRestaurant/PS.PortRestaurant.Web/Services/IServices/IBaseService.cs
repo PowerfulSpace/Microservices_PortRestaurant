adfba3868ee8c07e1c8cf59fdab911396ed99045
Alexander <sany199574@gmail.com>
2024-01-06 04:42:16 +0300

    Добавлен интерфейс  - IBaseService

---
+++ src/PortRestaurant/PS.PortRestaurant.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,11 @@
+﻿using PS.PortRestaurant.Web.Models;
+using PS.PortRestaurant.Web.Models.Dto;
+
+namespace PS.PortRestaurant.Web.Services.IServices
+{
+    public interface IBaseService : IDisposable
+    {
+        public ResponseDto ResponseModel { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
