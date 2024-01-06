4a4c9813298f1dd05c3e7cbe61eba5c1d59fbea7
Alexander <sany199574@gmail.com>
2024-01-06 05:40:52 +0300

    Реализован класс  - ProductService

--- src/PortRestaurant/PS.PortRestaurant.Web/Services/IServices/IProductService.cs
+++ src/PortRestaurant/PS.PortRestaurant.Web/Services/IServices/IProductService.cs
@@ -2,10 +2,10 @@
 
 namespace PS.PortRestaurant.Web.Services.IServices
 {
-    public interface IProductService
+    public interface IProductService : IBaseService
     {
         public Task<T> GetAllProductsAsync<T>();
-        public Task<T> GetProductAsync<T>(Guid id);
+        public Task<T> GetProductByIdAsyncAsync<T>(Guid id);
         public Task<T> CreateProductAsync<T>(ProductDto productDto);
         public Task<T> UpdateProductAsync<T>(ProductDto productDto);
         public Task<T> DeleteProductAsync<T>(Guid id);
---
+++ src/PortRestaurant/PS.PortRestaurant.Web/Services/ProductService.cs
@@ -0,0 +1,89 @@
+﻿using PS.PortRestaurant.Web.Models;
+using PS.PortRestaurant.Web.Models.Dto;
+using PS.PortRestaurant.Web.Services.IServices;
+
+namespace PS.PortRestaurant.Web.Services
+{
+    public class ProductService : BaseService,IProductService
+    {
+        public ResponseDto ResponseModel { get; set; }
+
+        private readonly IHttpClientFactory _clientFactory;
+
+        public ProductService(IHttpClientFactory clientFactory) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+
+        public async Task<T> GetAllProductsAsync<T>()
+        {
+            return await this.SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.ProductAPIBase + "api/ProductAPI",
+                AccessToken = ""
+            });
+        }
+
+        public async Task<T> GetProductByIdAsyncAsync<T>(Guid id)
+        {
+            return await this.SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.ProductAPIBase + "api/ProductAPI/" + id,
+                AccessToken = ""
+            });
+        }
+
+
+
+        public async Task<T> CreateProductAsync<T>(ProductDto productDto)
+        {
+            return await this.SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = productDto,
+                Url = SD.ProductAPIBase + "api/ProductAPI",
+                AccessToken = ""
+            });
+        }
+
+        public async Task<T> UpdateProductAsync<T>(ProductDto productDto)
+        {
+            return await this.SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.PUT,
+                Data = productDto,
+                Url = SD.ProductAPIBase + "api/ProductAPI",
+                AccessToken = ""
+            });
+        }
+
+        public async Task<T> DeleteProductAsync<T>(Guid id)
+        {
+            return await this.SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.DELETE,
+                Url = SD.ProductAPIBase + "api/ProductAPI/" + id,
+                AccessToken = ""
+            });
+        }
+
+
+
+
+        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
