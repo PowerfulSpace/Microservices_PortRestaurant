2504a72a55fb95126d7a843c565714df3f84a14a
Alexander <sany199574@gmail.com>
2024-01-06 04:32:30 +0300

    Добавлены модели в веб приложение:  - Product  - Category И dto модели  - ProductDto  - ResponseDto

---
+++ src/PortRestaurant/PS.PortRestaurant.Web/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace PS.PortRestaurant.Web.Models
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public List<Product> Products { get; set; } = new List<Product>();
+    }
+}
---
+++ src/PortRestaurant/PS.PortRestaurant.Web/Models/Dto/ProductDto.cs
@@ -0,0 +1,14 @@
+﻿namespace PS.PortRestaurant.Web.Models.Dto
+{
+    public class ProductDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public Guid? CategoryId { get; set; }
+        public Category? Category { get; set; }
+    }
+}
