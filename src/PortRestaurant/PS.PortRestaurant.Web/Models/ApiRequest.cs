38e4bbc6db0222fc3b4c2a30ac7dddedcd9745ee
Alexander <sany199574@gmail.com>
2024-01-06 04:38:19 +0300

    Добавлена модель  - ApiRequest

---
+++ src/PortRestaurant/PS.PortRestaurant.Web/Models/ApiRequest.cs
@@ -0,0 +1,12 @@
+﻿using static PS.PortRestaurant.Web.SD;
+
+namespace PS.PortRestaurant.Web.Models
+{
+    public class ApiRequest
+    {
+        public ApiType ApiType { get; set; }
+        public string Url { get; set; }
+        public object Data { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
