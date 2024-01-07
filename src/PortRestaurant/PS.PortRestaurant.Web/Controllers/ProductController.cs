5f2e8ca663f575a51c0583b36869f7501dd939f1
Alexander <sany199574@gmail.com>
2024-01-07 09:07:16 +0300

    реализован метод  - ProductIndex

--- src/PortRestaurant/PS.PortRestaurant.Web/Controllers/ProductController.cs
+++ src/PortRestaurant/PS.PortRestaurant.Web/Controllers/ProductController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PS.PortRestaurant.Web.Models.Dto;
+using PS.PortRestaurant.Web.Services.IServices;
 
 namespace PS.PortRestaurant.Web.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
         {
-            return View();
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDto> list = new List<ProductDto>();
+            var response = await _productService.GetAllProductsAsync<ResponseDto>();
+
+            if(response.Result != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+
+            return View(list);
         }
     }
 }
