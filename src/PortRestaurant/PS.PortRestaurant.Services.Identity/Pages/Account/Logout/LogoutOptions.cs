ee1d6cd5e66f60b95e83edfd4296fe98bd5f7b8a
Alexander <sany199574@gmail.com>
2024-01-15 16:05:06 +0300

    Удалён проблем, из-за неправильной работы сервиса

--- src/PortRestaurant/PS.PortRestaurant.Services.Identity/Pages/Account/Logout/LogoutOptions.cs
+++
@@ -1,8 +0,0 @@
-
-namespace PortRestaurant.Pages.Logout;
-
-public class LogoutOptions
-{
-    public static bool ShowLogoutPrompt = true;
-    public static bool AutomaticRedirectAfterSignOut = false;
-}
