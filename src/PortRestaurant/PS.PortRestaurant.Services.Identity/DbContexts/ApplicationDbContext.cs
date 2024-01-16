80764e8e01f4e5d3f9cf568459eb70007be8879e
Alexander <sany199574@gmail.com>
2024-01-16 23:21:55 +0300

    Добавлен класс  -ApplicationUser
    Исправлен пользователь для работы с бд

--- src/PortRestaurant/PS.PortRestaurant.Services.Identity/DbContexts/ApplicationDbContext.cs
+++ src/PortRestaurant/PS.PortRestaurant.Services.Identity/DbContexts/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PS.PortRestaurant.Services.Identity.Models;
 
 namespace PS.PortRestaurant.Services.Identity.DbContexts
 {
 
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
         {
