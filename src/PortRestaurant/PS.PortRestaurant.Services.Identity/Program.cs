f44e111b496a71d4579603e96c02168c8eff9ec6
Alexander <sany199574@gmail.com>
2024-01-17 00:03:21 +0300

    Добавлена система идентификации в проект

--- src/PortRestaurant/PS.PortRestaurant.Services.Identity/Program.cs
+++ src/PortRestaurant/PS.PortRestaurant.Services.Identity/Program.cs
@@ -1,10 +1,17 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PS.PortRestaurant.Services.Identity.DbContexts;
+using PS.PortRestaurant.Services.Identity.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services
+    .AddDbContext<ApplicationDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services
+    .AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddRazorPages();
 
