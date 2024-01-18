58e44a469bd8c00c35f4850aa9d25121c138ee0c
Alexander <sany199574@gmail.com>
2024-01-18 23:56:36 +0300

    Добавлена миграция изменения свойств пользователя

--- src/PortRestaurant/PS.PortRestaurant.Services.Identity/Models/ApplicationUser.cs
+++ src/PortRestaurant/PS.PortRestaurant.Services.Identity/Models/ApplicationUser.cs
@@ -4,5 +4,7 @@ namespace PS.PortRestaurant.Services.Identity.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FistName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
     }
 }
