b1b3d5ed7a6d5bba3ecfadbb9c1caaa9bfbfac6f
Alexander <sany199574@gmail.com>
2024-01-23 00:19:36 +0300

    Исправил ошибку связанную с неправильной авторизацией

--- src/PortRestaurant/PS.PortRestaurant.Web/Services/BaseService.cs
+++ src/PortRestaurant/PS.PortRestaurant.Web/Services/BaseService.cs
@@ -33,7 +33,7 @@ namespace PS.PortRestaurant.Web.Services
                     message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data), Encoding.UTF8, "application/json");
                 }
 
-                if(string.IsNullOrEmpty(apiRequest.AccessToken))
+                if(!string.IsNullOrEmpty(apiRequest.AccessToken))
                 {
                     client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",apiRequest.AccessToken);
                 }
